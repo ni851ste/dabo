d60b842bfcb3bb3f19c680ee39fa647dd098e046
caroweng <caro.weng@gmx.net>
2020-05-07 16:31:29 +0200

    created Homepage, added bootstrap

---
+++ frontend/src/components/Homepage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        {{msg}}
+        Homepage
+
+
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Prop, Vue } from 'vue-property-decorator';
+
+    @Component
+    export default class HomePage extends Vue {
+        @Prop() private msg!: string;
+    }
+</script>
+
+<style scoped>
+</style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from "vue-router";
+import Homepage from "@/components/Homepage.vue";
+import "bootstrap-vue"
+import "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 Vue.config.productionTip = false
 
 Vue.use(VueRouter);
 const routes = [
+  {path: '/', component: Homepage },
 ];
 const router = new VueRouter({
   routes: routes
