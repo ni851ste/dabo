81d6f096e029e6ee43d1322716be841187c74ed9
caroweng <caro.weng@gmx.net>
2020-05-11 15:43:47 +0200

    basic Layout, category cards

--- frontend/src/components/Homepage.vue
+++ frontend/src/components/Homepage.vue
@@ -1,16 +1,26 @@
 <template>
     <div>
-        {{msg}}
-        Homepage
+        <NavigationBar msg="hallo"></NavigationBar>
+        <CategoryView></CategoryView>
+<!--        {{msg}}-->
+<!--        Homepage-->
 
+<!--        <router-link to="/">-->
+<!--            <button type="button" class="btn btn-dark " >-->
+<!--                Test-->
+<!--            </button>-->
+<!--        </router-link>-->
 
     </div>
 </template>
 
 <script lang="ts">
     import { Component, Prop, Vue } from 'vue-property-decorator';
-
-    @Component
+    import NavigationBar from "@/components/NavigationBar.vue";
+    import CategoryView from "@/components/CategoryView.vue";
+    @Component({
+        components: {CategoryView, NavigationBar}
+    })
     export default class HomePage extends Vue {
         @Prop() private msg!: string;
     }
