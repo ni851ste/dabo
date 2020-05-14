c60929cad6051007b7400e5796d4dd41d3957115
Helen Nganga <helennganga53@gmail.com>
2020-05-14 13:03:30 +0200

    add testDeleteArticel

--- backend/app/model/article/Article.java
+++ backend/app/model/article/Article.java
@@ -6,8 +6,9 @@ import java.util.*;
 public class Article {
     Map<Integer, List<String>> article = new HashMap<>();
     int globalIdCounter = 0;
-    //TODO add  Date[] notAvailableDate, image
-    public boolean createArticle(String name, String description, String insertionDate, String notAvailableDate, String rating, String location, String category  ) {
+    //TODO add   image
+    public boolean createArticle(String name, String description, String insertionDate,
+                                 String notAvailableDate, String rating, String location, String category  ) {
         int localIdCounter = this.globalIdCounter;
         List<String> values = new ArrayList<>();
         values.add(name);
--- backend/test/model/article/ArticleTest.java
+++ backend/test/model/article/ArticleTest.java
@@ -10,18 +10,28 @@ public class ArticleTest {
 
     @Test
     public void testCreateANewArticle() {
-        Article test = new Article();
+        Article testArticel = new Article();
 
-        assertTrue(test.createArticle("Bierpong-tisch","only used twice",
+        assertTrue(testArticel.createArticle("Bierpong-tisch","only used twice",
                 "20.02.1996", "18.04.1993","5 Stars",
                 "Berlin", "Sports & Leisure"));
-        assertEquals("Bierpong-tisch",test.article.get(0).get(0));
-        assertEquals("only used twice",test.article.get(0).get(1));
-        assertEquals("20.02.1996",test.article.get(0).get(2));
-        assertEquals("18.04.1993",test.article.get(0).get(3));
-        assertEquals("5 Stars",test.article.get(0).get(4));
-        assertEquals("Berlin",test.article.get(0).get(5));
-        assertEquals("Sports & Leisure",test.article.get(0).get(6));
+        assertEquals("Bierpong-tisch",testArticel.article.get(0).get(0));
+        assertEquals("only used twice",testArticel.article.get(0).get(1));
+        assertEquals("20.02.1996",testArticel.article.get(0).get(2));
+        assertEquals("18.04.1993",testArticel.article.get(0).get(3));
+        assertEquals("5 Stars",testArticel.article.get(0).get(4));
+        assertEquals("Berlin",testArticel.article.get(0).get(5));
+        assertEquals("Sports & Leisure",testArticel.article.get(0).get(6));
+    }
+
+    @Test
+    public void testDeleteArticel(){
+        Article testArticel = new Article();
+
+        assertTrue(testArticel.createArticle("Bierpong-tisch","only used twice",
+                "20.02.1996", "18.04.1993","5 Stars",
+                "Berlin", "Sports & Leisure"));
+        assertTrue(testArticel.deleteArticle(0));
 
     }
 
