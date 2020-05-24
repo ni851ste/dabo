862efced8d245d9e690ea185819e5cd5e5e0b275
Helen Nganga <helennganga53@gmail.com>
2020-05-24 16:20:31 +0200

    add id

--- backend/app/model/article/ArticleHttpAdapter.java
+++ backend/app/model/article/ArticleHttpAdapter.java
@@ -1,5 +1,6 @@
 package model.article;
 
+import model.article.Article;
 import play.mvc.Result;
 
 import static play.mvc.Results.ok;
@@ -7,15 +8,15 @@ import static play.mvc.Results.ok;
 public class ArticleHttpAdapter {
     Article adabter = new Article();
 
-    public Result createUserAdapter(){
+    public Result createArticleAdapter(){
         adabter.createArticle("Bierpong-tisch","only used twice",
                 "20.02.1996", "18.04.1993","5 Stars",
                 "Berlin", "Sports & Leisure");
         return ok();
     }
 
-    public Result deleteUserAdapter(){
-        adabter.deleteArticle(0);
+    public Result deleteArticleAdapter(int id){
+        adabter.deleteArticle(id);
         return ok();
     }
 
