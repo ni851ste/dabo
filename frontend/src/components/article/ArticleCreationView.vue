148b8bcc66bc948ac20f51fcb76b414a950826b5
JuliaBurri <julia.burri@web.de>
2020-05-27 15:25:13 +0200

    fixed image upload for ArticleCreationView

--- frontend/src/components/article/ArticleCreationView.vue
+++ frontend/src/components/article/ArticleCreationView.vue
@@ -2,61 +2,78 @@
     <div>
         <NavigationBar msg="hallo"></NavigationBar>
         <div class="contentLayout my-buttons">
-            <div class="borderbox b-container b-form-checkbox-group" >
-                <h4> Allgemein</h4>
-
+            <div class="borderbox b-container b-form-checkbox-group">
+                <h1> Allgemein</h1>
                 <div class="form-group">
                     <label for="articleNameInput">Artikelbezeichnung:</label>
                     <input type="name" class="form-control" id="articleNameInput">
                 </div>
-
-                <div class="form-contol">
-                    <label for="articleDescriptionInput">Artikelbeschreibung:</label>
-                    <textarea class="form-control" id="articleDescriptionInput" rows="3" maxlength='250'></textarea>
-                </div>
-                <br>
-                <p>Zugehörigkeit:</p>
-                    <b-container fluid>
-                        <b-row>
-                            <b-form-checkbox-group
-                                    id="categories"
-                                    v-model="selected"
-                                    :options="options"
-                                    name="category"
-                            ></b-form-checkbox-group>
-                        </b-row>
-                    </b-container>
-                </div>
-
-            <div class="borderbox file-upload-wrapper">
-                <h4>Bilder</h4>
                 <div>
-                    <b-form-file v-model="file" ref="file-input" class="mb-2" multiple
-                            :state="Boolean(file)"
-                            placeholder="Choose a file or drop it here..."
-                    ></b-form-file>
+                    <label>Artikelbeschreibung</label>
+                    <textarea class="form-control" v-on:keyup="countdown" v-model="message"></textarea>
+                    <p class='text-right text-small' v-bind:class="{'text-danger': hasError }">{{charCount + '/' +
+                        maxCount}}</p>
                 </div>
-            </div>
-
-            <div class="borderbox">
-                <h4>Verfügbarkeit</h4>
-
+                <p>Zugehörigkeit:</p>
                 <b-container fluid>
                     <b-row>
-                        <b-col>
-                            <b-form-datepicker id="example-datepicker" v-model="value" class="mb-2" placeholder="von"></b-form-datepicker>
-                        </b-col>
-                        <b-col>
-                            <b-form-datepicker id="example-datepicker" v-model="value" class="mb-2" placeholder="bis"></b-form-datepicker>
-                        </b-col>
+                        <b-form-checkbox-group
+                                id="categories"
+                                v-model="selected"
+                                :options="categories"
+                                name="category"
+                        ></b-form-checkbox-group>
                     </b-row>
-
                 </b-container>
-
             </div>
 
             <div class="borderbox">
-                <h4>Standort</h4>
+                <h1>Bilder</h1>
+                <div class="b-container">
+                    <b-row>
+                        <div b-col>
+                            <div class="imageUpload">
+                                <base-image-input v-model="imageFile"/>
+                            </div>
+                        </div>
+                        <div b-col>
+                            <div class="imageUpload">
+                                <base-image-input v-model="imageFile"/>
+                            </div>
+                        </div>
+                        <div b-col>
+                            <div class="imageUpload">
+                                <base-image-input v-model="imageFile"/>
+                            </div>
+                        </div>
+                        <div b-col>
+                            <div class="imageUpload">
+                                <base-image-input v-model="imageFile"/>
+                            </div>
+                        </div>
+                        <div b-col>
+                            <div class="imageUpload">
+                                <base-image-input v-model="imageFile"/>
+                            </div>
+                        </div>
+                    </b-row>
+                </div>
+            </div>
+
+            <div class="borderbox">
+                <h1>Verfügbarkeit</h1>
+                <date-range-picker
+                        :startDate="startDate"
+                        :endDate="endDate"
+                        @update="console.log(value)"
+                        :locale-data="locale"
+                >
+                    <div slot="input" slot-scope="picker">...</div>
+                </date-range-picker>
+            </div>
+
+            <div class="borderbox">
+                <h1>Standort</h1>
                 <div class="form-group">
                     <label for="countryInput">Land:</label>
                     <input type="string" class="form-control" id="countryInput">
@@ -67,48 +84,66 @@
                 </div>
             </div>
 
-            <div class="borderbox">
+            <div class="addButton">
                 <div>
-                    <button type="button" class="btn btn-primary btn-block">Artikel hinzufügen</button>                </div>
+                    <button type="button" class="btn add-btn-primary btn-block">Artikel hinzufügen</button>
                 </div>
             </div>
 
+        </div>
     </div>
 </template>
 
-    <script lang="ts">
+<script lang="ts">
     import NavigationBar from "@/components/NavigationBar.vue";
-    import { Component, Vue } from 'vue-property-decorator';
+    import BaseImageInput from "@/components/BaseImageInput.vue";
+    import {Component, Vue} from 'vue-property-decorator';
+
     @Component({
-        components: {NavigationBar}
+        components: {NavigationBar, BaseImageInput}
     })
 
+    export default class ArticleCreationView extends Vue {
+        maxCount: number = 200;
+        message: string = '';
+        hasError: boolean = false;
+        charCount: number = 0
 
-    export default class ArticleCreationView extends Vue{
-        data() {
-            return {
-                selected: [], // Must be an array reference!
-                options: [
-                    { text: 'Auto & Motorrad', value: 'car' },
-                    { text: 'Elektronik', value: 'electronics' },
-                    { text: 'Garten', value: 'garden' },
-                    { text: 'Haushalt', value: 'household' },
-                    { text: 'Kleidung', value: 'chlothes' },
-                    { text: 'Sport & Freizeit', value: 'sports' },
-                    { text: 'Unterhaltung', value: 'entertainment' },
-                    { text: 'Werkzeug', value: 'tools' }
-                ]
-            }
+        countdown() {
+            this.charCount = this.message.length;
+            this.hasError = this.message.length > this.maxCount;
         }
 
+        selected: [] = [];
+        categories = [
+            {text: 'Auto & Motorrad', value: 'car'},
+            {text: 'Elektronik', value: 'electronics'},
+            {text: 'Garten', value: 'garden'},
+            {text: 'Haushalt', value: 'household'},
+            {text: 'Kleidung', value: 'chlothes'},
+            {text: 'Sport & Freizeit', value: 'sports'},
+            {text: 'Unterhaltung', value: 'entertainment'},
+            {text: 'Werkzeug', value: 'tools'}
+        ]
+
+        data() {
+            return {
+                imageFile: null
+            }
+        }
     }
 </script>
 
 <style scoped>
 
+    h1 {
+        font-size: x-large;
+        font-weight: bold;
+    }
+
     .borderbox {
-        border:#484848 solid 1px;
-        width: 80%;
+        border: #d0f2e1 solid 2px;
+        width: 100%;
         margin-top: 15px;
         margin-left: auto;
         margin-right: auto;
@@ -122,10 +157,23 @@
         alignment: left;
     }
 
-    .btn-primary {
+    .addButton {
         background-color: #d0f2e1;
         border-color: #d0f2e1;
-        color:#484848;
-
+        color: #484848;
+        width: 100%;
+        margin-top: 15px;
+        margin-bottom: 25px;
+        border: solid #d0f2e1;
+        border-radius: 8px;
     }
+
+    textarea.form-control {
+        height: 100px;
+    }
+
+    .text-small {
+        font-size: 0.9em;
+    }
+
 </style>
