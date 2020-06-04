6ac3e624416c08ce70d564cffd6d2051af979173
HelenNganga <helennganga53@gmail.com>
2020-06-04 16:17:01 +0200

    addArticleAdapter works with request

--- backend/app/model/article/ArticleHttpAdapter.java
+++ backend/app/model/article/ArticleHttpAdapter.java
@@ -1,21 +1,40 @@
 package model.article;
 
-import play.mvc.Http;
-import play.mvc.Result;
-import play.mvc.Controller;
+import com.fasterxml.jackson.databind.JsonNode;
 
+import play.mvc.*;
+
+
+import java.io.IOException;
+import play.libs.concurrent.HttpExecutionContext;
+import play.mvc.*;
+
+import javax.inject.Inject;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 
 import static play.mvc.Results.ok;
 
 
+
+
+
 public class ArticleHttpAdapter {
     Article adapter = new Article();
-//TODO
-    // Json to Java
-    public Result addArticleAdapter(){
-        int artikleId = adapter.addArticle("Bierpong-tisch","only used twice",
-                "20.02.1996", "Berlin");
-        return ok("Artikel ID:" + artikleId);
+
+    private HttpExecutionContext httpExecutionContext;
+
+    public Result addArticleAdapter(Http.Request request){
+    System.out.println("request:" + request.toString());
+    System.out.println("request" + request.body().asJson());
+//    JsonNode json = request.body().asJson();
+//    String name  = json.get("name").toString();
+//    String description = json.get("description").toString();
+//    String location = json.get("location").toString();
+//    String insertionDate = json.get("insertionDate").toString();
+//
+//    int artikelID = adapter.addArticle(name, description, insertionDate, location);
+        return ok("Artikel ID:" );
     }
 
     public Result deleteArticleAdapter(int id){
@@ -25,27 +44,15 @@ public class ArticleHttpAdapter {
         return ok("Artikle:" + id + " würde nicht gelöscht");
     }
 
-    public Result showArticleAdapter(int id){
-        adapter.showArticle(id);
-        String testArtikle = "{\n" +
-                "  \"name\": \"Georg\",\n" +
-                "  \"alter\": 47,\n" +
-                "  \"verheiratet\": false,\n" +
-                "  \"beruf\": null,\n" +
-                "  \"kinder\": [\n" +
-                "    {\n" +
-                "      \"name\": \"Lukas\",\n" +
-                "      \"alter\": 19,\n" +
-                "      \"schulabschluss\": \"Gymnasium\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"name\": \"Lisa\",\n" +
-                "      \"alter\": 14,\n" +
-                "      \"schulabschluss\": null\n" +
-                "    }\n" +
-                "  ]\n" +
-                "}";
-        return ok(testArtikle).as("application/json");
+
+    public CompletionStage<Result> showArticleAdapter(int id) throws IOException {
+
+        return adapter.showArticle(id)
+                .thenApplyAsync(
+                     answer -> {
+                         return ok(answer);
+                     },
+                             httpExecutionContext.current());
     }
 
 }
