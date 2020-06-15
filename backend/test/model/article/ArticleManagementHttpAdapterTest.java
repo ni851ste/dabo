fff9dcf5e6f92dbb9288d862a578e1cc88c853b1
HelenNganga <helennganga53@gmail.com>
2020-06-15 19:11:36 +0200

    Test createJson add jsons to test resources

--- backend/test/model/article/ArticleManagementHttpAdapterTest.java
+++ backend/test/model/article/ArticleManagementHttpAdapterTest.java
@@ -1,48 +1,60 @@
 package model.article;
 
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import play.mvc.Http;
 import play.mvc.Result;
+import play.test.Helpers;
 import play.test.WithApplication;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 import static play.mvc.Http.Status.OK;
-import static play.test.Helpers.*;
+import static play.test.Helpers.POST;
+import static play.test.Helpers.route;
+
 
 public class ArticleManagementHttpAdapterTest extends WithApplication {
 
-//    @Test
-//    public void testRequestAddArtikel() throws IOException {
-//
-//        String input = "{" +
-//                "\"name\": \"BierPong3\"," +
-//                "\"description\": \"guter Zustand\", " +
-//                "\"image\": \"image\"," +
-//                "\"location\": \"Berlin\"," +
-//                "\"insertionDate\": \"20.02.13\"" +
-//                "}";
-//
-//        ObjectMapper mapper = new ObjectMapper();
-//        JsonFactory factory = mapper.getFactory();
-//        JsonParser jp = factory.createParser(input);
-//        JsonNode jsonNode = mapper.readTree(jp);
-//
-//        //System.out.println(jsonNode.toString());
-//
-//        Http.RequestBuilder request = new Http.RequestBuilder()
-//                .method(POST)
-//                .uri("/users/articles/create")
-//                .bodyJson(jsonNode);
-//
-//        Result result = route(app,request);
-//        assertEquals(OK, result.status());
-//    }
+    @Test
+    public void testcreateArticleJson() throws IOException {
+
+        ObjectMapper mapper = new ObjectMapper();
+        File from = new File("test\\resources\\createArticle.json");
+        JsonNode createArticleJson = mapper.readTree(from);
+
+        Http.RequestBuilder requestshow =new Http.RequestBuilder()
+                .method(POST)
+                .uri("/users/articles/create")
+                .bodyJson(createArticleJson);
+
+        Result result = route(app,requestshow);
+
+        assertEquals(OK, result.status());
+
+
+        String resultJsonToString = Helpers.contentAsString(result, mat);
+
+        File testFile = new File("test\\target\\createArtikleTest.json");
+        testFile.createNewFile();
+        FileWriter fileWriter = new FileWriter(testFile);
+
+        fileWriter.write(resultJsonToString);
+        fileWriter.flush();
+        fileWriter.close();
+
+        File actuallyFile = new File("test\\resources\\getArticle.json");
+
+        JsonNode testJson = mapper.readTree(testFile);
+        JsonNode actuallyJson = mapper.readTree(actuallyFile);
+
+        assertEquals(actuallyJson,testJson);
+
+    }
 //
 //    @Test
 //    public void testRequestDeleteArtikel() throws IOException {
@@ -57,26 +69,6 @@ public class ArticleManagementHttpAdapterTest extends WithApplication {
 
 //    @Test
 //    public void testRequestShowArtikel() throws IOException {
-//        String input = "{" +
-//                "\"name\": \"BierPong3\"," +
-//                "\"description\": \"guter Zustand\", " +
-//                "\"image\": \"image\"," +
-//                "\"location\": \"Berlin\"," +
-//                "\"insertionDate\": \"20.02.13\"" +
-//                "}";
-//
-//        ObjectMapper mapper = new ObjectMapper();
-//        JsonFactory factory = mapper.getFactory();
-//        JsonParser jp = factory.createParser(input);
-//        JsonNode jsonNode = mapper.readTree(jp);
-//
-//
-//        Http.RequestBuilder requestshow =new Http.RequestBuilder()
-//                .method(POST)
-//                .uri("/users/articles/create")
-//                .bodyJson(jsonNode);
-//
-//        System.out.println("show" + requestshow.body().toString());
 //
 //        Http.RequestBuilder request = new Http.RequestBuilder()
 //                .method(GET)
@@ -85,7 +77,8 @@ public class ArticleManagementHttpAdapterTest extends WithApplication {
 //        System.out.println("show" + request.body().toString());
 //
 //
-//        Result result = route(app,request);
-//        assertEquals(OK, result.status());
+//
 //    }
+
+
 }
