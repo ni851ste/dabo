1a7ca34b249f859b746075640308a9e64a56beb0
JuliaBurri <julia.burri@web.de>
2020-06-20 11:48:43 +0200

    changed address

--- frontend/src/components/user/Adress.ts
+++ frontend/src/components/user/Adress.ts
@@ -1,13 +1,15 @@
-export default class Adress {
+export default class Address {
     street: string;
     plz: string;
     city: string;
+    country: string;
     streetVisible: boolean
 
-    constructor(street: string, plz: string, city:string, streetVisible: boolean) {
+    constructor(street: string, plz: string, city: string, country: string, streetVisible: boolean) {
         this.street = street;
         this.plz = plz;
         this.city = city;
+        this.country = country;
         this.streetVisible = streetVisible
     }
 }
--- frontend/src/components/user/User.ts
+++ frontend/src/components/user/User.ts
@@ -1,4 +1,4 @@
-import Adress from "@/components/user/Adress";
+import Address from "@/components/user/Adress";
 import Rating from "@/components/rating/Rating";
 
 export default class User {
@@ -7,7 +7,7 @@ export default class User {
     password: string;
     firstname: string;
     lastname: string;
-    adress: Adress;
+    address: Address;
     picture: string;
     lastNameVisible: boolean;
 
@@ -18,7 +18,7 @@ export default class User {
     friendsId: number[];
 
     constructor(uuid: number, email: string, password: string, firstname: string, lastname: string, picture: string,
-                lastnameVisible: boolean, adress: Adress, ratings: Rating[], insertedArticlesId: number[],
+                lastnameVisible: boolean, address: Address, ratings: Rating[], insertedArticlesId: number[],
                 borrowedArticlesId: number[], pinnedArticledId: number[], friendsId: number[]) {
         this.uuid = uuid;
         this.email = email;
@@ -27,7 +27,7 @@ export default class User {
         this.lastname = lastname;
         this.picture = picture;
         this.lastNameVisible = lastnameVisible;
-        this.adress = adress
+        this.address = address
         this.ratings = ratings;
         this.insertedArticlesId = insertedArticlesId;
         this.borrowedArticlesId = borrowedArticlesId;
