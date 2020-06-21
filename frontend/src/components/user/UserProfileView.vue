09f744c25bc9a9483e187023e89231a1e4c64ebe
JuliaBurri <julia.burri@web.de>
2020-06-21 14:22:03 +0200

    first implementation of user profile (not connected to backend yet)

--- frontend/src/components/user/UserProfileView.vue
+++ frontend/src/components/user/UserProfileView.vue
@@ -100,7 +100,10 @@
             this.articles.push(article2);
 
             let rating: Rating = new Rating(3, "Sehr zuverlässig und unkompliziert.", 1, new Date())
-            let rating2: Rating = new Rating(1, "nicht empfehlenswert. wvebORUHWADJFHBhfjrbhjbjnvhgfvdfghujkj", 2, new Date())
+            let rating2: Rating = new Rating(1, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. " +
+                "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took " +
+                "a galley of type and scrambled it to make a type specimen book. "
+                , 2, new Date())
             this.ratings.push(rating)
             this.ratings.push(rating2)
 
