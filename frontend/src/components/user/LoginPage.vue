1e722c3bf02000d0e02b19217229145ca19b29f8
caroweng <caro.weng@gmx.net>
2020-08-09 11:44:51 +0200

    Added alert for register first

--- frontend/src/components/user/LoginPage.vue
+++ frontend/src/components/user/LoginPage.vue
@@ -36,22 +36,23 @@
 
             </div>
             <b-alert v-model="showAlert" class="alert" variant="danger" dismissible>Login fehlgeschlagen.</b-alert>
+            <b-alert v-model="this.showLoginFirstAlert" class="alert" variant="warning" dismissible>Sie müssen sich erst einloggen.</b-alert>
         </div>
     </div>
 </template>
 
 <script lang="ts">
     import NavigationBar from "../NavigationBar.vue";
-    import {Component, Vue} from 'vue-property-decorator';
-    import {Md5} from 'ts-md5/dist/md5';
+    import {Component, Prop, Vue} from 'vue-property-decorator';
     import LoginService from "@/components/services/LoginService";
-    import $ from "jquery"
 
     @Component({
         components: {NavigationBar}
     })
 
     export default class LoginPage extends Vue {
+        @Prop() private showLoginFirstAlert!: boolean;
+
         passwordVisible: boolean;
         validateInput: boolean;
         email: string;
